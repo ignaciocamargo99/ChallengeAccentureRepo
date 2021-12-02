2ea593e4638ccfa23adc20d4e926af7ceb691fa1
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2021-12-01 22:08:33 -0300

    EndPoints users and comments

--- ChallengeAccenture/src/main/java/com/challenge/apirest/controllers/ExternalApiController.java
+++ ChallengeAccenture/src/main/java/com/challenge/apirest/controllers/ExternalApiController.java
@@ -5,15 +5,32 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin("*") 
 @RestController
 @RequestMapping("/") 
 public class ExternalApiController {
 	
+	static final String uri = "https://jsonplaceholder.typicode.com";
+	
 	@GetMapping("example")
 	public ResponseEntity<Object> index(){
 		return ResponseEntity.ok("OK");
+	}
+	
+	@GetMapping("users")
+	private String getUsers() {
+		RestTemplate restTemplate = new RestTemplate();
+		String users = restTemplate.getForObject(uri + "/users", String.class);
+		return users;
+	}
+	
+	@GetMapping("comments")
+	private String getComments() {
+		RestTemplate restTemplate = new RestTemplate();
+		String comments = restTemplate.getForObject(uri+ "/comments", String.class);
+		return comments;
 	}
 
 }
