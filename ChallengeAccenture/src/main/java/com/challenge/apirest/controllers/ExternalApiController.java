4eb98147083262c2d9d8efa24e856bbd6e4550ce
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2021-12-02 17:07:23 -0300

    Endpoint commentsByName

--- ChallengeAccenture/src/main/java/com/challenge/apirest/controllers/ExternalApiController.java
+++ ChallengeAccenture/src/main/java/com/challenge/apirest/controllers/ExternalApiController.java
@@ -3,7 +3,9 @@ package com.challenge.apirest.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,8 +31,20 @@ public class ExternalApiController {
 	@GetMapping("comments")
 	private String getComments() {
 		RestTemplate restTemplate = new RestTemplate();
-		String comments = restTemplate.getForObject(uri+ "/comments", String.class);
+		String comments = restTemplate.getForObject(uri + "/comments", String.class);
 		return comments;
+	}
+	
+	@GetMapping("photos/{user}")
+	private ResponseEntity<Object> findPhotosByUser(@PathVariable("user") String user){
+		return ResponseEntity.ok("Ok");
+	}
+	
+	@GetMapping("commentsByName")
+	private String getCommentsByName(@RequestParam String name) {
+		RestTemplate restTemplate = new RestTemplate();
+		String commentsByName = restTemplate.getForObject(uri + "/comments?name=" + name, String.class);
+		return commentsByName;
 	}
 
 }
