60e409102b879738e9b33f5bf8771216f168d7a4
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2021-12-03 12:02:31 -0300

    Exception handling and empty data

--- ChallengeAccenture/src/main/java/com/challenge/apirest/controllers/ExternalApiController.java
+++ ChallengeAccenture/src/main/java/com/challenge/apirest/controllers/ExternalApiController.java
@@ -17,107 +17,104 @@ import com.challenge.apirest.utils.exceptions.BadRequestException;
 import com.challenge.apirest.utils.exceptions.NotFoundException;
 
 @RestController
-@RequestMapping("/") 
-public class ExternalApiController implements ErrorController{
+@RequestMapping("/")
+public class ExternalApiController implements ErrorController {
 	static final String uri = "https://jsonplaceholder.typicode.com";
-	
+
 	@GetMapping("users")
 	private String getUsers() {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			String users = restTemplate.getForObject(uri + "/users", String.class);
-			return users;	
-		}
-		catch(Exception e){
+			return users;
+		} catch (Exception e) {
 			return e.getMessage();
 		}
-		
 	}
-	
+
 	@GetMapping("comments")
 	private String getComments() {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			String comments = restTemplate.getForObject(uri + "/comments", String.class);
 			return comments;
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			return e.getMessage();
 		}
-		
+
 	}
-	
+
 	@GetMapping("userPhotos")
-	private String getUserPhotos(@RequestParam String userId){
+	private String getUserPhotos(@RequestParam String userId) {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			String userAlbum = restTemplate.getForObject(uri + "/albums?userId=" + userId, String.class);
 			JSONArray albums = new JSONArray(userAlbum);
-			
-			ArrayList<Integer> albumIds = new ArrayList<>(); 
-			for(int i = 0; i < albums.length(); i ++) {
+
+			ArrayList<Integer> albumIds = new ArrayList<>();
+			for (int i = 0; i < albums.length(); i++) {
 				JSONObject json = albums.getJSONObject(i);
 				albumIds.add(json.getInt("id"));
 			}
-			
-			String userPhotos = restTemplate.getForObject(uri + "/photos?albumId=" + albumIds.get(0) 
-			+ "&albumId=" + albumIds.get(1) + "&albumId=" + albumIds.get(2) + "&albumId=" + albumIds.get(3) +
-			"&albumId=" + albumIds.get(4) + "&albumId=" + albumIds.get(5) + "&albumId=" + albumIds.get(6) +
-			"&albumId=" + albumIds.get(7) + "&albumId=" + albumIds.get(8) + "&albumId=" + albumIds.get(9), String.class);
-			
+
+			String userPhotos = restTemplate.getForObject(
+					uri + "/photos?albumId=" + albumIds.get(0) + "&albumId=" + albumIds.get(1) + "&albumId="
+							+ albumIds.get(2) + "&albumId=" + albumIds.get(3) + "&albumId=" + albumIds.get(4)
+							+ "&albumId=" + albumIds.get(5) + "&albumId=" + albumIds.get(6) + "&albumId="
+							+ albumIds.get(7) + "&albumId=" + albumIds.get(8) + "&albumId=" + albumIds.get(9),
+					String.class);
+
 			return userPhotos;
-			
-		}
-		catch(Exception e) {
-			throw new BadRequestException("You must pass an INTEGER for the userId");
+
+		} catch (Exception e) {
+			throw new BadRequestException(e.getMessage() + ". Please, pass an INTEGER between 1 and 10 for userId");
 		}
 	}
-	
+
 	@GetMapping("commentsByName")
 	private String getCommentsByName(@RequestParam String name) {
-		try {			
+		try {
 			RestTemplate restTemplate = new RestTemplate();
 			String commentsByName = restTemplate.getForObject(uri + "/comments?name=" + name, String.class);
-			return commentsByName;
-		}
-		catch(Exception e) {
-			throw new BadRequestException("You must pass an STRING for the name");
+
+			if (!commentsByName.equals("[]")) {
+				return commentsByName;
+			} else {
+				throw new Exception("No data found");
+			}
+
+		} catch (Exception e) {
+			throw new BadRequestException(e.getMessage() + ". Please, pass an STRING for comment name");
 		}
 	}
-	
+
 	@GetMapping("commentsByUser")
 	private String getCommentsByUser(@RequestParam String userId) {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			String userPosts = restTemplate.getForObject(uri + "/posts?userId=" + userId, String.class);
-			
-//			if(!userPosts.equals("")) {
-			
-				JSONArray posts = new JSONArray(userPosts);
-				
-				
-				ArrayList<Integer> postsIds = new ArrayList<>(); 
-				
-				for(int i = 0; i < posts.length(); i ++) {
-					JSONObject json = posts.getJSONObject(i);
-					postsIds.add(json.getInt("id"));
-				}
-			
-				
-				String userComments = restTemplate.getForObject(uri + "/comments?postId=" + postsIds.get(0) 
-				+ "&postId=" + postsIds.get(1) + "&postId=" + postsIds.get(2) + "&postId=" + postsIds.get(3) +
-				"&postId=" + postsIds.get(4) + "&postId=" + postsIds.get(5) + "&postId=" + postsIds.get(6) +
-				"&postId=" + postsIds.get(7) + "&postId=" + postsIds.get(8) + "&postId=" + postsIds.get(9), String.class);
-				
-				return userComments;	
-//			}
-		}
-		catch(Exception e) {
-			throw new BadRequestException("You must pass an INTEGER for the userId");
+
+			JSONArray posts = new JSONArray(userPosts);
+
+			ArrayList<Integer> postsIds = new ArrayList<>();
+
+			for (int i = 0; i < posts.length(); i++) {
+				JSONObject json = posts.getJSONObject(i);
+				postsIds.add(json.getInt("id"));
+			}
+
+			String userComments = restTemplate.getForObject(uri + "/comments?postId=" + postsIds.get(0) + "&postId="
+					+ postsIds.get(1) + "&postId=" + postsIds.get(2) + "&postId=" + postsIds.get(3) + "&postId="
+					+ postsIds.get(4) + "&postId=" + postsIds.get(5) + "&postId=" + postsIds.get(6) + "&postId="
+					+ postsIds.get(7) + "&postId=" + postsIds.get(8) + "&postId=" + postsIds.get(9), String.class);
+
+			return userComments;
+
+		} catch (Exception e) {
+			throw new BadRequestException(e.getMessage() + ". Please, pass an INTEGER between 1 and 10 for userId");
 		}
 
 	}
-	
 
 	@GetMapping("/error")
 	private String error404(HttpServletRequest request) {
--- ChallengeAccenture/src/main/java/com/challenge/apirest/utils/exceptions/NotFoundException.java
+++ ChallengeAccenture/src/main/java/com/challenge/apirest/utils/exceptions/NotFoundException.java
@@ -1,12 +1,12 @@
 package com.challenge.apirest.utils.exceptions;
 
 /** 
- * Status 404 
+ * Status 404
  * */
 
 public class NotFoundException extends RuntimeException{
 	
-	private static final String DESCRIPTION = "Not Found Exception (400)";
+	private static final String DESCRIPTION = "Not Found Exception (404)";
 	
 	public NotFoundException(String message) {
 		super(DESCRIPTION + ". " + message);
